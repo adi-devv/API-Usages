7db8f8e87d33da32bd3b4dfc5abef5888e87eb82
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-05-06 23:47:29 +0530

    Add files via upload

--- StockUpdates/main.py
+++ StockUpdates/main.py
@@ -21,20 +21,24 @@ stk_params = {
     "apikey": api_key,
 
 }
-
-resp = requests.get(endpoint, params=stk_params)
-data = resp.json()["Time Series (Daily)"]
-dataL = [v for (i, v) in data.items()]
-
-closingP = [v["4. close"] for v in dataL]
-
-diff = float(closingP[0])-float(closingP[1])
-emoji = None
-if diff>=0:
-    emoji = "🔺"
-else:
-    emoji = "🔻"
-
-perc = round((diff)*100/float(closingP[1]))
-
 print(str(perc)+"%")
+
+if abs(perc)>=.5:
+    news_params = {
+        "apiKey": "x",
+        "qinTitle": company,
+    }
+
+    resp2 = requests.get(news_ep, params=news_params)
+    articles = resp2.json()["articles"][:1]
+
+    msgs = [f"{stock}:{emoji}{perc}%\nHeadline:{v['title']}\nBrief:{v['description']}" for v in articles]
+    client = Client(twilio_sid, twilio_token)
+
+    for msg in msgs:
+        msg = client.messages \
+            .create(
+                body=msg,
+                from_="+x",
+                to="+x"
+            )
