fb8187326e7bb428dfe046101c16501cfe92ab97
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-05-07 16:10:03 +0530

    Add files via upload

---
+++ rainyday.py
@@ -0,0 +1,42 @@
+import os
+
+import requests
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+f_url = "https://api.openweathermap.org/data/2.5/forecast"
+key = "get the key"
+acc_sid = "xx"
+auth_token = "x"
+
+weather_params = {
+    "lat": 25.572491,
+    "lon": 91.310760,
+    "appid": key,
+    "cnt": 4
+}
+
+resp = requests.get(f_url, params=weather_params)
+resp.raise_for_status()
+data = resp.json()
+
+will_rain = False
+
+for hourdata in data["list"]:
+    condition = hourdata["weather"][0]["id"]
+    if condition <= 700:
+        will_rain = True
+
+if will_rain:
+    proxyClient = TwilioHttpClient()
+    proxyClient.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(acc_sid, auth_token, http_client=proxyClient)
+    msg = client.messages \
+        .create(
+            body="Gonna rain!",
+            from_="+x",
+            to="+number"
+        )
+
+print(msg.status)
