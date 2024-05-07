f3519a911ee23132fd7ec542b2d7f6f4feeda6b8
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-05-07 16:19:22 +0530

    Add files via upload

--- IntensityGraph.py
+++ IntensityGraph.py
@@ -5,16 +5,16 @@ token = "x"
 usn = "x"
 
 user_params = {
-    "token": token ,
+    "token": token,
     "username": usn,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-#resp = requests.post(url = pix_ep, json=user_params)
+# resp = requests.post(url = pix_ep, json=user_params)
 # print(resp.text)
 
-graph_ep = f"{pix_ep}/{usn}"
+graph_ep = f"{pix_ep}/{usn}/graphs"
 graph_config = {
     "id": "graph1",
     "name": "WalkingGraph",
@@ -24,5 +24,9 @@ graph_config = {
 
 }
 
-resp = requests.post(url = graph_ep, json = graph_config)
+headers = {
+    "X-USER-TOKEN": token
+}
+
+resp = requests.post(url=graph_ep, json=graph_config, headers=headers)
 print(resp.text)
