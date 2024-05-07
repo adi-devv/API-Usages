f2d8ddcd5ba0d8e9c7a94551f4b12b0887738283
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-05-07 20:11:12 +0530

    Add files via upload

--- IntensityGraph.py
+++ IntensityGraph.py
@@ -1,9 +1,10 @@
 import requests
+from datetime import datetime
 
 pix_ep = "https://pixe.la/v1/users"
-token = "x"
-usn = "x"
-
+token = "svk24iunfwnad"
+usn = "aaddit"
+gID = "graph1"
 user_params = {
     "token": token,
     "username": usn,
@@ -16,10 +17,10 @@ user_params = {
 
 graph_ep = f"{pix_ep}/{usn}/graphs"
 graph_config = {
-    "id": "graph1",
+    "id": gID,
     "name": "WalkingGraph",
     "unit": "Steps",
-    "type": "int",
+    "type": "float",
     "color": "sora",
 
 }
@@ -28,5 +29,17 @@ headers = {
     "X-USER-TOKEN": token
 }
 
-resp = requests.post(url=graph_ep, json=graph_config, headers=headers)
+# resp = requests.post(url=graph_ep, json=graph_config, headers=headers)
+# print(resp.text)
+
+pix_epp = f"{pix_ep}/{usn}/graphs/{gID}"
+today = datetime.now()
+
+pix_data = {
+    "date": input("Date in YYYYMMDD format?\n"),#today.strftime("%Y%m%d"),
+    "quantity": input("How many steps buddy?\n"),
+}
+
+resp = requests.post(url=pix_epp, json=pix_data, headers=headers)
 print(resp.text)
+
