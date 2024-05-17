d3a44a4443c1b05caef429a0a8462b183da9a2aa
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-05-17 13:05:08 +0530

    Add files via upload

--- sky-scanner/FlightFinder.py
+++ sky-scanner/FlightFinder.py
@@ -1,5 +1,7 @@
 import requests, json
+from flight_data import FlightData
 
+x
 sheet_ep = "https://api.sheety.co/x/flightDeals/prices"
 h2 = {
     "Authorization": "Bearer x"
@@ -8,11 +10,13 @@ h2 = {
 sheet_resp = requests.get(sheet_ep, headers=h2)
 sheet_data = sheet_resp.json()["prices"]
 
-file = open("Airports.txt","r+")
+file = open("Airports.txt", "r+")
 airports = json.load(file)
 
-if sheet_data[0]['iataCode']:
-    for v in sheet_data:
+print('runnning')
+
+for v in sheet_data:
+    if v['iataCode'] == '':
         v['iataCode'] = airports.get(v['city'], 'Missing')
         resp = requests.put(
             url=f"{sheet_ep}/{v['id']}",
@@ -22,3 +26,30 @@ if sheet_data[0]['iataCode']:
             headers=h2
         )
         print(resp.text)
+
+for v in sheet_data:
+    slices = [
+        {
+            "origin": "BHO",
+            "destination": v['iataCode'],
+            "departure_date": "2024-06-21"
+        },
+        {
+            "origin": v['iataCode'],
+            "destination": "BHO",
+            "departure_date": "2024-07-21"
+        }
+    ]
+    print('a')
+
+    v['lowestPrice'] = F.get_prices(slices)
+
+    resp = requests.put(
+        url=f"{sheet_ep}/{v['id']}",
+        json={
+            "price": {"lowestPrice": v["lowestPrice"]}
+        },
+        headers=h2
+    )
+    print(resp.json())
+    break
